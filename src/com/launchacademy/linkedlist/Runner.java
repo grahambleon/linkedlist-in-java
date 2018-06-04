9466478a2eeda7566a06134444a992e2fd111d60
Graham Gilpatric <grahamwithag@gmail.com>
2018-06-04 19:59:13 -0400

    Cleaned up the runner

--- src/com/launchacademy/linkedlist/Runner.java
+++ src/com/launchacademy/linkedlist/Runner.java
@@ -6,38 +6,28 @@ public class Runner {
     Node theNode = new Node("The");
     Node orangeNode = new Node("Orange");
     Node catNode = new Node("Cat");
-//    Node thereNode = new Node("There");
     
     LinkedList sentence = new LinkedList(theNode);
     theNode.next = orangeNode;
     orangeNode.next = catNode;
-//    sentence.pushNode(thereNode);
     
     System.out.println(sentence.toString());
     
     //output "The Orange Furry Cat"  
     Node furryNode = new Node("Furry");
     sentence.insertAfter(orangeNode, furryNode);
-//    sentence.appendNode(new Node("I'm"));
     System.out.println(sentence.toString());
     
+    //output "The Orange Furry Dog"
     sentence.deleteNode(catNode);
-    sentence.deleteNode(furryNode);
-    sentence.appendNode(new Node("Kitten"));
+    sentence.appendNode(new Node("Dog"));
     System.out.println(sentence.toString());
     
-    System.out.println(sentence.length());
-
     
-//    //output "The Orange Furry Dog"
-//    sentence.deleteNode(catNode);
-//    sentence.appendNode(new Node("Dog"));
-//    System.out.println(sentence.toString());
-//    
-//    
-//    //output "An Orange Furry Dog"
-//    sentence.deleteNode(theNode);
-//    sentence.pushNode(new Node("An"));
-//    System.out.println(sentence.toString());
+    //output "An Orange Furry Dog"
+    sentence.deleteNode(theNode);
+    sentence.pushNode(new Node("An"));
+    System.out.println(sentence.toString());
+    System.out.println(sentence.length());
   }
 }
